c19faf3ce935cf185508936326634baae9fd80d3
greyspectrum <hello@greyspectrum.io>
2022-05-19 11:24:34 -0400

    iterating over a hash map

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,29 @@ fn main() {
 
     let mut scores: HashMap<_, _> =
         teams.into_iter().zip(initial_scores.into_iter()).collect();
+
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    // Both field_name and field_value are
+    // invalid after this point; they
+    // have been moved, since String does
+    // not implement the copy trait
+
+    // Accessing values in a hash map
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name);
+
+    // Iterating over key/value pairs in
+    // a hash map
+    for (key, value) in &scores {
+        println!("{}: {}", key, value);
+    }
 }
